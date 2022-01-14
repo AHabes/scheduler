abd27828fb45ba2312a8bd4fe09c938cd82e3b74
Abdullah Habes <abdulla096@hotmail.com>
2022-01-13 21:43:14 -0500

    fix bug update the routes to access the api server

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -21,7 +21,7 @@ export function useApplicationData() {
     };
 
     return new Promise((resolve, reject) => {
-      axios.put(`http://localhost:8001/api/appointments/${id}`, {interview: interview}
+      axios.put(`/api/appointments/${id}`, {interview: interview}
       ).then(res => {
 
         const days = updateSpots(appointments);
@@ -46,7 +46,7 @@ export function useApplicationData() {
     };
 
     return new Promise((resolve, reject) => {
-      axios.delete(`http://localhost:8001/api/appointments/${id}`
+      axios.delete(`/api/appointments/${id}`
       ).then(res => {
         const days = updateSpots(appointments);
         setState({...state, appointments, days});
@@ -73,9 +73,9 @@ export function useApplicationData() {
 
   useEffect(() => {
     Promise.all(
-      [axios.get('http://localhost:8001/api/days'),
-        axios.get('http://localhost:8001/api/appointments'),
-        axios.get('http://localhost:8001/api/interviewers')])
+      [axios.get('/api/days'),
+        axios.get('/api/appointments'),
+        axios.get('/api/interviewers')])
       .then(all => {
         setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
 
